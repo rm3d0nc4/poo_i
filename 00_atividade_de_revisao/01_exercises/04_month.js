1bdae532d17562fe17eb874d27918e320c683cad
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2022-08-11 08:06:47 -0300

    fixed exercise (04)

--- 00_atividade_de_revisao/01_exercises/04_month.js
+++ 00_atividade_de_revisao/01_exercises/04_month.js
@@ -2,16 +2,17 @@ import { input, print } from "../utils.js"
 
 function main() {
     const months = [
-        'January', 'February', 'March', 
-        'April', 'May', 'June',
-        'July', 'August', 'September',
-        'October', 'November', 'December']
+        ['January', 31], ['February', 28], ['March', 31], 
+        ['April', 30], ['May', 31], ['June', 30],
+        ['July', 31], ['August', 31], ['September', 30],
+        ['October', 31], ['November', 30], ['December', 31]]
+
     
     const number_selected_month = Number(input('Enter number of the month to be selected: '))
 
     const selected_month = months[number_selected_month-1]
 
-    print(`Selected month: ${selected_month}`)
+    print(`Selected month: ${selected_month[0]} (${selected_month[1]} days)`)
 }
 
 main()
